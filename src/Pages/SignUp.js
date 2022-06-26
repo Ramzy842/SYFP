3da6148f1024d8f89874838e9894ec679c27e5e7
Ramzy842 <ramzychahbani@gmail.com>
2022-06-26 22:35:01 +0100

    added validation for signup page

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -16,7 +16,10 @@ const SignUp = () => {
   const [pw, setPw] = useState("");
   const [pwConfirm, setPwConfirm] = useState("");
   const [error, setError] = useState("");
-  const { auth, loginVariants, setLoadingHero } = GlobalContext();
+  const [emailErr, setEmailErr] = useState("");
+  const [pwErr, setPwErr] = useState("");
+  const [pwConfErr, setPwConfErr] = useState("");
+  const { auth, setLoadingHero } = GlobalContext();
   const history = useHistory();
 
   const _isMounted = useRef(true);
@@ -36,7 +39,30 @@ const SignUp = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    if (pw === pwConfirm && _isMounted.current) {
+    if (email.length === 0) {
+      setPwErr("");
+      setPwConfErr("");
+      setEmailErr("Please Enter an Email");
+      setTimeout(() => {
+        setEmailErr("");
+      }, 4000);
+    } else if (pw.length === 0) {
+      setEmailErr("");
+      setPwConfErr("");
+      setPwErr("Please Enter a Password");
+      setTimeout(() => {
+        setPwErr("");
+      }, 4000);
+    } 
+    else if (pwConfirm.length === 0) {
+      setEmailErr("");
+      setPwErr("");
+      setPwConfErr("Please Confirm Your Password");
+      setTimeout(() => {
+        setPwConfErr("");
+      }, 4000);
+    }
+    else if (pw === pwConfirm && _isMounted.current) {
       setLoading(true);
 
       await createUserWithEmailAndPassword(auth, email, pw)
@@ -53,15 +79,20 @@ const SignUp = () => {
         .catch(() => {
           if (_isMounted.current && pw.length < 6 && pwConfirm.length < 6) {
             setLoading(false);
-            setError(" Your password should be at least 6 characters long");
+            setEmailErr("");
+            setPwConfErr("")
+            setPwErr(" Your password should be at least 6 characters long");
             setTimeout(() => {
-              setError("");
+              setPwErr("");
             }, 4000);
           } else if (_isMounted.current) {
             setLoading(false);
-            setError("This email is already in use");
+            
+            setPwConfErr("")
+            setPwErr("");
+            setEmailErr("This email is either already in use or isn't valid");
             setTimeout(() => {
-              setError("");
+              setEmailErr("");
             }, 3000);
           }
 
@@ -69,9 +100,9 @@ const SignUp = () => {
         });
     } else if (_isMounted.current && pw !== pwConfirm) {
       setLoading(false);
-      setError("Please make sure your passwords match");
+      setPwConfErr("Please make sure your passwords match");
       setTimeout(() => {
-        setError("");
+        setPwConfErr("");
       }, 3000);
     }
   };
@@ -85,47 +116,73 @@ const SignUp = () => {
         <img src={github} alt="github" />
       </a>
       <div className="lg:flex lg:flex-col   justify-center items-center">
-      <form action="" className="container sm:w-1/2 lg:w-1/3 max-w-4xl mx-auto px-4" data-aos="fade-in">
-        <input
-          type="text"
-          placeholder="Email"
-          className="bg-input h-12 rounded-md pl-4 w-full mb-4 placeholder:text-main placeholder:font-extralight shadow-md outline-focus"
-          required
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Password"
-          className="bg-input h-12 rounded-md pl-4 w-full mb-4 placeholder:text-main placeholder:font-extralight shadow-md outline-focus"
-          required
-          onChange={(e) => setPw(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Confirm Password"
-          className="bg-input h-12 rounded-md pl-4 w-full placeholder:text-main placeholder:font-extralight shadow-md outline-focus"
-          required
-          onChange={(e) => setPwConfirm(e.target.value)}
-        />
-        <div className="flex justify-between mt-5">
-          <p className="w-36 text-white text-sm">
-            Already have an account?{" "}
-            <Link to="/" className="underline">
-              Sign In
-            </Link>
-          </p>
+        <form
+          action=""
+          className="container sm:w-1/2 lg:w-1/3 max-w-4xl mx-auto px-4"
+          data-aos="fade-in"
+        >
+          {error && (
+            <p className="text-red-600 text-center px-8 max-w-4xl mb-4 mx-auto ">
+              {error}
+            </p>
+          )}
+          <div id="email">
+            <input
+              type="text"
+              placeholder="Email"
+              className="bg-input h-12 rounded-md pl-4 w-full mb-4 placeholder:text-main placeholder:font-extralight shadow-md outline-focus"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {emailErr && (
+              <p className="text-red-600  max-w-4xl mb-4 mx-auto ">
+                {emailErr}
+              </p>
+            )}
+          </div>
+          <div id="password">
+            <input
+              type="text"
+              placeholder="Password"
+              className="bg-input h-12 rounded-md pl-4 w-full mb-4 placeholder:text-main placeholder:font-extralight shadow-md outline-focus"
+              required
+              onChange={(e) => setPw(e.target.value)}
+            />
+            {pwErr && (
+              <p className="text-red-600  max-w-4xl mb-4 mx-auto ">{pwErr}</p>
+            )}
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Confirm Password"
+              className="bg-input h-12 rounded-md pl-4 w-full placeholder:text-main placeholder:font-extralight shadow-md outline-focus"
+              required
+              onChange={(e) => setPwConfirm(e.target.value)}
+            />
+            {pwConfErr && (
+              <p className="text-red-600  max-w-4xl mb-4 mx-auto ">{pwConfErr}</p>
+            )}
+          </div>
 
-          <button
-            type="submit"
-            className=" flex items-center bg-form-btn rounded-md p-2 shadow-md text-base font-semibold text-white "
-            onClick={handleSubmit}
-          >
-            Sign Up <img src={signin} className="w-6 h-6 ml-2" alt="signin" />
-          </button>
-        </div>
-      </form>
+          <div className="flex justify-between mt-5">
+            <p className="w-36 text-white text-sm">
+              Already have an account?{" "}
+              <Link to="/" className="underline">
+                Sign In
+              </Link>
+            </p>
+
+            <button
+              type="submit"
+              className=" flex items-center bg-form-btn rounded-md p-2 shadow-md text-base font-semibold text-white "
+              onClick={handleSubmit}
+            >
+              Sign Up <img src={signin} className="w-6 h-6 ml-2" alt="signin" />
+            </button>
+          </div>
+        </form>
       </div>
-      
     </div>
   );
 };
